551f398a4881366f14da5a2387ddbc0d18b505e0
barnazaka <barnazaka@gmail.com>
2022-10-16 20:12:29 +0100

    second commit

---
+++ navbar.php
@@ -0,0 +1,21 @@
+
+<style>
+</style>
+<nav id="sidebar" class='mx-lt-5 bg-warning' >
+		
+		<div class="sidebar-list">
+
+				<a href="index.php?page=home" class="nav-item nav-home"><span class='icon-field'><i class="fa fa-home"></i></span> Home</a>
+				<a href="index.php?page=loans" class="nav-item nav-loans"><span class='icon-field'><i class="fa fa-file-invoice-dollar"></i></span> Token Payment</a>	
+				<a href="index.php?page=payments" class="nav-item nav-payments"><span class='icon-field'><i class="fa fa-money-bill"></i></span> Payments</a>
+				<a href="index.php?page=borrowers" class="nav-item nav-borrowers"><span class='icon-field'><i class="fa fa-user-friends"></i></span> Buy Token</a>		
+				<?php if($_SESSION['login_type'] == 1): ?>
+				<a href="index.php?page=users" class="nav-item nav-users"><span class='icon-field'><i class="fa fa-users"></i></span> Users</a>
+				
+			<?php endif; ?>
+		</div>
+
+</nav>
+<script>
+	$('.nav-<?php echo isset($_GET['page']) ? $_GET['page'] : '' ?>').addClass('active')
+</script>
